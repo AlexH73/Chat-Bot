ab056fd9a095079d920dae947db29eea4cc8d201
Alex_H <ewebotah@gmail.com>
2025-01-25 08:54:26 +0100

    Updated

--- order_logic.py
+++ order_logic.py
@@ -1,12 +1,6 @@
-import json
-from ui import choose_language, choose_item, confirm_order, change_order
+from data import LANGUAGES, CATEGORIES
+from ui import choose_item, confirm_order, change_order
 from utils import clear_screen
-
-with open('languages.json', 'r', encoding='utf-8') as f:
-    LANGUAGES = json.load(f)
-
-with open('menu.json', 'r', encoding='utf-8') as f:
-    CATEGORIES = json.load(f)
 
 def start_order(selected_language, order=None):
     """
@@ -14,8 +8,9 @@ def start_order(selected_language, order=None):
 
     Args:
         selected_language (str): Выбранный язык.
-         order (list): Список словарей, представляющий заказ.
+        order (list): Список словарей, представляющий заказ.
     """
+    clear_screen()
     lang = LANGUAGES[selected_language]
     # Получаем языковой словарь для выбранного языка.
     print(lang["welcome"])
@@ -42,12 +37,21 @@ def start_order(selected_language, order=None):
                 # Задаем вопрос, хочет ли пользователь что-то изменить
                 choice = input(f"{lang['your_choice']}")
                 # Получаем ответ от пользователя
+                if choice == "0":
+                    # Выводим сообщение благодарности.
+                    print(lang["thank_you"])
                 if choice == "5":
                     # Если пользователь ответил "5" вызываем функцию изменения заказа
-                    order = change_order(order, lang, selected_language)
+                    change = change_order(order, lang, selected_language)
+                    if change == "back":
+                      continue
+                    elif change == "add_item":
+                      continue
+                    else:
+                        order = change
                 else:
-                    # Если пользователь ответил не "5"  завершаем заказ и выходим из цикла
-                    break
+                   # Если пользователь ответил не "5"  завершаем заказ и выходим из цикла
+                      break
             else:
                 # Если заказ пустой, выходим из цикла.
                 break
